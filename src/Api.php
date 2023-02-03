93e684b28e414a148c2df6845a2b1cf531996b1a
Mateo <orehurcomito@gmail.com>
2023-02-03 15:58:33 -0500

    remove property client

--- src/Api.php
+++ src/Api.php
@@ -13,41 +13,29 @@ class Api
     public const BASE_URL = 'https://graph.facebook.com/%s/%s/';
     public const VERSION = 'v13.0';
 
-    private Request $client;
-    private string $token;
-    private int $phoneId;
-
     public array $payload = ['messaging_product' => 'whatsapp'];
     public string $endpoint;
 
-    public function __construct(string $token, int $phoneNumberId)
-    {
-        $this->client = new Request();
-        $this->token = $token;
-        $this->phoneId = $phoneNumberId;
-    }
+    public function __construct(private string $token, private int $phoneNumberId)
+    {}
 
     public function send(string $endpoint, array $payload, string $version = self::VERSION): \stdClass
     {
-        $this->endpoint = sprintf(self::BASE_URL, $version, $this->phoneId);
+        $this->endpoint = sprintf(self::BASE_URL, $version, $this->phoneNumberId);
         $payload = json_encode($this->addOpt($payload)->payload);
 
-
-        $this->client->init($this->endpoint, [
-          CURLOPT_HTTPHEADER => [
-            'Authorization: Bearer ' . $this->token,
-            'Content-Type: application/json'
-          ],
-          CURLOPT_POSTFIELDS => $payload,
-          CURLOPT_POST => true
-        ]);
+		$request = Request::POST($this->endpoint, $payload)
+			->addOpts([
+				CURLOPT_HTTPHEADER => [
+					'Authorization: Bearer ' . $this->token,
+					'Content-Type: application/json'
+				]
+			]);
 
 
-
-        $response = $this->client->setMethod('POST')
-          ->Run($endpoint);
-
-        return $response->toJson(true)->getBody();
+		return $request->run($endpoint)
+			->toJson(true, true)
+			->getBody();
     }
 
     public function addOpt(array $opt): static
